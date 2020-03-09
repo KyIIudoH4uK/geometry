6fc588e1256a75a1ce0e6f2774b28d6f3ba95456
Maxim <charleselvisbrooks@gmail.com>
2020-03-09 16:46:59 +0700

    Made structures

---
+++ geometry.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+typedef struct {
+    Point center;
+    int radius;
+    float area;
+    float perimetr;
+} Circle;
+
+int main() {
+    
+}
